6d0a71bcfc2f5710420665490ae06a9561ebacad
Ethan Ewing <ethanewing52@gmail.com>
2018-01-31 14:13:48 -0500

    updated model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,20 @@
 class Song < ApplicationRecord
+	attr_accessor :title, :artist, :album, :genre, :plays, :year
+
 	# Associates an attachment and a blob. When the song is destroyed
 	# they are purged by default (models destroyed, and resource files
 	# deleted).
 	has_one_attached :mp3
+
+	def initialize(title=' ', artist=' ', album=' ', genre=' ', plays=0, year=0)
+		# Note: add exception handling for invalid values
+		# Note2: also, should have an ID attribute -- not sure best way to implement this yet tho
+		# (i.e., through class variable, or something else)
+		@title = title
+		@artist = artist
+		@album = album
+		@genre = genre
+		@plays = plays
+		@year = year
+	end
 end
